9818c62ef38e1996668ea0c0572f3f5a23ae4d9d
zathizh <sathish.bowatta@gmail.com>
2023-05-06 04:38:01 +0800

    Adding 15/16

---
+++ modules/16_sec_u_group_changed.py
@@ -0,0 +1,98 @@
+#! /usr/bin/python3
+
+import os
+import sys
+import jinja2
+import configparser
+
+from datetime import datetime
+
+framework_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
+os.chdir(framework_path)
+sys.path.append('./classes')
+
+from emailreport import EmailReport
+from elasticstack import ElasticStack
+
+## Gloabl variable, if needs to compare against something
+THRESHOLD = 0
+
+## MAIN CONFIGURATION FILE PATH
+MAIN_CONFIG = "configs/main.cfg"
+TEMPLATE_FILE = "table_template.html"
+ITEM_PATH_EXCLUSIONS = "exclusions/users/sec_universal_group.lst"
+
+def main():
+    # create elastic stack object
+    estack = ElasticStack()
+
+    # handle log index
+    config = configparser.ConfigParser(delimiters=('='))
+    config.read(MAIN_CONFIG)
+
+    # handle jinja2 templates
+    templateLoader = jinja2.FileSystemLoader(searchpath="templates")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    template = templateEnv.get_template(TEMPLATE_FILE)
+
+    index = config.get('CONFIGURATIONS', 'INDEX')
+
+    ## query needs to overwrite by each script.
+    estack.query = query = {
+            "query": {
+                "bool": {
+                    "must": [
+                        {
+                            "match": {
+                                "winlog.event_id": "4755"
+                                }
+                            }
+                        ],
+                    "filter": [
+                        {
+                            "range": {
+                                "@timestamp": {
+                                    "gte": "now-5m"
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+
+    # required a api call modification based on the query
+    result = estack.es.search(index=index, body=estack.query,  size=1000)
+
+    # logic to trigger tge emails. set subject and body to send the emails to the listed recepients
+    count = result['hits']['total']['value']
+    if count > THRESHOLD:
+        excluded_items = list(map(str.strip, open(ITEM_PATH_EXCLUSIONS, 'r').readlines()))
+        hits = result['hits']['hits']
+
+        header = ["Timestamp", "Computer Name", "Group Name", "Subject User Name"]
+        artifacts = [header]
+        counter = 0
+        for record in hits:
+            # from python 3.7 onwards datetime.fromisoformat is available
+            print(record)
+            source = record['_source']
+            event_data = source['winlog']['event_data']
+            item = event_data['TargetUserName']
+            _timestamp = datetime.strptime(source['@timestamp'], "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%H:%M:%S")
+
+            if item not in excluded_items:
+                artifacts.append([_timestamp, source['winlog']['computer_name'], event_data['TargetUserName'], event_data['SubjectUserName']])
+                counter+=1
+
+        if counter :
+            table = template.render(artifacts=artifacts)
+            
+            org = "[ " + config.get('GENERAL', 'ORG') + " ] "
+            mailbody = "{counter}/{count} security-enabled universal group change events were detected during last 5 minutes\n\n".format(counter=counter, count=count)
+            em = EmailReport(subject=org + "Alert - A security-enabled universal group was changed [Excluding the defined exclusions]", body=mailbody, table=table)
+            em.sendEmail()
+
+if __name__ == '__main__':
+    main()
+
